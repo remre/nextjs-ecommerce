91c20e2ba9239317d4ae228344873f52757e2226
remre <basarremre@outlook.com>
2024-01-02 17:27:10 +0300

    feature(add-product): add server side actions

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,12 +1,38 @@
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
 export const metadata = {
   title: "Add-Product",
 };
 
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const ImageUrl = formData.get("ImageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !ImageUrl || !price) {
+    throw Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: {
+      name,
+      description,
+      ImageUrl,
+      price,
+    },
+  });
+
+  redirect("/");
+}
 export default function addProductPage() {
   return (
     <div>
       <h1 className="mb- text-xl font-bold"> Add Product</h1>
-      <form>
+      <form action={addProduct}>
         <input
           className="mb- input input-bordered w-full"
           required
