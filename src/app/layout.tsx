ef041c1258eb4b5be90705925dcd949afa4b9b8d
remre <basarremre@outlook.com>
2024-01-29 14:47:27 +0300

    fix: import navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./navbar/Navbar";
 import Footer from "./Footer";
 import SessionProvider from "./SessionProvider";
+import Navbar from "./navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
