664eab3ccb4abaf68500cb936afa035213d716d9
remre <basarremre@outlook.com>
2024-02-05 23:51:56 +0300

    feat: cancel sign for add product templ

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -5,17 +5,17 @@ import { prisma } from "@/lib/db/prisma";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
-export const metadata = {
-  title: "Add-Product",
-  metadataBase: "http://localhost:3000",
-};
+// export const metadata = {
+//   title: "Add-Product",
+//   metadataBase: "http://localhost:3000",
+// };
 
 async function addProduct(formData: FormData) {
   "use server";
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    redirect("/api/auth/signin?callbackUrl=/add-product");
-  }
+  // const session = await getServerSession(authOptions);
+  // if (!session) {
+  //   redirect("/api/auth/signin?callbackUrl=/add-product");
+  // }
 
   const name = formData.get("name")?.toString();
   const description = formData.get("description")?.toString();
@@ -38,10 +38,10 @@ async function addProduct(formData: FormData) {
   redirect("/");
 }
 export default async function addProductPage() {
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    redirect("/api/auth/signin?callbackUrl=/add-product");
-  }
+  // const session = await getServerSession(authOptions);
+  // if (!session) {
+  //   redirect("/api/auth/signin?callbackUrl=/add-product");
+  // }
   return (
     <div>
       <h1 className="mb-3 text-xl font-bold"> Add Product</h1>
