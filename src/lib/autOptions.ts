3d3d84917134f4913e11f4c1effb6cae81a8688c
remre <basarremre@outlook.com>
2024-03-31 17:20:08 +0300

    feat: update user db model

--- src/lib/autOptions.ts
+++ src/lib/autOptions.ts
@@ -6,6 +6,7 @@ import { Adapter } from "next-auth/adapters";
 import { prisma } from "@/lib/db/prisma";
 import NextAuth from "next-auth/next";
 import { env } from "@/lib/env";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { mergeAnanymousCartIntoUserCart } from "@/lib/db/cart";
 import { PrismaClient } from "@prisma/client";
 
@@ -16,6 +17,24 @@ export const authOptions: NextAuthOptions = {
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
+
+    // CredentialsProvider({
+    //   name: 'Credentials',
+    //   credentials: {
+    //     username: { label: "Username", type: "text", placeholder: "jsmith" },
+    //     password: {  label: "Password", type: "password" }
+    //   },
+    //   authorize: async (credentials) => {
+    //     // Add logic here to find user from your database
+    //     // Example with prisma
+    //     const user = await prisma.user.findUnique({where: {name: credentials.username}});
+    //     if (user && user.password === credentials.password) {
+    //       return Promise.resolve(user)
+    //     } else {
+    //       return Promise.resolve(null)
+    //     }
+    //   }
+    // })
   ],
   callbacks: {
     session({ session, user }) {
